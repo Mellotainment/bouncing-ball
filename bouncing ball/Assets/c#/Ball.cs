df37ce3553d7f6d70c335708f3b36a352f81ba03
Mellotainment <winstoncc56@gmail.com>
2016-09-27 05:32:04 -0400

    Add files via upload

---
+++ bouncing ball/Assets/c#/Ball.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Ball : MonoBehaviour {
+
+
+	public GUIText scoreText;
+	public int score;
+
+		
+
+
+	
+
+		void Start (){
+			
+			score = 0;
+			UpdateScore ();
+		
+			}
+		
+
+		public void AddScore (int newScoreValue)  
+
+	
+{
+	        score += newScoreValue;
+			UpdateScore ();
+		}
+
+		void UpdateScore ()
+		{
+			scoreText.text = "Score: " + score;
+		Debug.Log ("you got a a friend in me");
+		}
+}
+
---
+++ bouncing ball/Assets/c#/Paddle.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public int scoreValue;
+	public Ball ball;
+	public AudioSource Zelda;
+	public AudioClip clip;
+
+	public void Awake (){
+
+		Zelda = GetComponent <AudioSource> ();
+	}
+
+	public void OnTriggerEnter(Collider other){
+		
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+	 //Update is called once per frame
+	void Update ()
+	{
+		Zelda.Play();
+
+		ball.AddScore (scoreValue);
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			transform.position += new Vector3 (0, 0, 1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			transform.position += new Vector3 (0, 0, -1);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (-1, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (1, 0, 0);
+		}
+
+	}
+}
+		
+	
+
